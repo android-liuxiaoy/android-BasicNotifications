6cb187eeeb9df3559310ca9d8076943a002505d8
liuxy <938651469@qq.com>
2019-02-19 19:02:37 +0800

    更新适配 8.0 之后的通知渠道和 gradle 配置

--- Application/src/main/java/com/example/android/basicnotifications/MainActivity.java
+++ Application/src/main/java/com/example/android/basicnotifications/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.basicnotifications;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
@@ -24,7 +28,27 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_layout);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // 通知渠道的id
+            String id = "1";
+            // 用户可以看到的通知渠道的名字.
+            CharSequence name = "notification channel";
+            // 用户可以看到的通知渠道的描述
+            String description = "notification description";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+            // 配置通知渠道的属性
+            mChannel.setDescription(description);
+            // 设置通知出现时的闪灯（如果 android 设备支持的话）
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            // 设置通知出现时的震动（如果 android 设备支持的话）
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            //最后在notificationManager中创建该通知渠道
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
     }
 
     /**
@@ -85,6 +109,7 @@ public class MainActivity extends Activity {
         builder.setContentTitle("BasicNotifications Sample");
         builder.setContentText("Time to learn about notifications!");
         builder.setSubText("Tap to view documentation about notifications.");
+        builder.setChannelId("1");
 
         // END_INCLUDE (build_notification)
 
